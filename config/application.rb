374d4c0f70f774a16f1ccd3b076cf7f2a26f6c7d
Patrick Stoica <pstoica@gatech.edu>
2013-02-25 19:34:07 -0500

    remove cors

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module Plantr
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
    
