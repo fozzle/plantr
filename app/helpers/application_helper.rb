81a7e2ee0d9c1d2be694d4ae8ed4456ab86277b5
Patrick Stoica <pstoica@gatech.edu>
2013-03-06 19:22:57 -0500

    add current password field, persist active tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def nav_link(link_text, link_path, *args)
-    class_name = current_page?(link_path) ? 'ui-btn-active' : ''
+    class_name = current_page?(link_path) ? 'ui-btn-active ui-state-persist' : ''
 
     link_to link_text, link_path, :class => class_name
   end
