a161dde6b917551a7c416eb68bc9f1bb84b9a629
Kyle Petrovich <kylepetrovich@gmail.com>
2013-03-11 23:30:18 -0400

    fixing my own mistakes oops

--- db/migrate/20130308204642_add_foreign_key_constraints_to_plant.rb
+++ db/migrate/20130308204642_add_foreign_key_constraints_to_plant.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyConstraintsToPlant < ActiveRecord::Migration
   def change
-  	add_foreign_key(:plants, :sensor_id, :sensors, :sens_id)
+  	add_foreign_key(:plants, :sensors, {primary_key: :sens_id})
   	add_foreign_key(:plants, :gardens)
 
   	add_index :plants, :sensor_id, :unique => true
