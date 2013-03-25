3a70e40cd618c01f8c393bbd805a11d604365602
Patrick Stoica <pstoica@gatech.edu>
2013-03-25 00:21:40 -0400

    fixing up the incoming SMS controller

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -2,16 +2,15 @@ class ReceiveTextController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-  	twilio_sid = "AC3d33aec0094782093e058f29c5093856"
-    twilio_token = "c7151b10ee5ad0318135974ad31e8cae"
-    
     message_body = params["Body"]
     from_number = params["From"]
     to_number = params["To"]
 
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
  
     garden = Garden.where(:phone => to_number).first
+    return if garden.nil?
+
     garden.users.each do |user|
     	next if user.phone.nil?
 
