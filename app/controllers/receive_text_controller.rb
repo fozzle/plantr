e36800e79d1932e2c923e751ad47781f58c6e265
Patrick Stoica <pstoica@gatech.edu>
2013-03-25 01:34:09 -0400

    wtffff

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -6,23 +6,5 @@ class ReceiveTextController < ActionController::Base
     message_body = params["Body"]
     from_number = params["From"]
     to_number = params["To"]
-
-    @twilio_client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
- 
-    garden = Garden.where(:phone => to_number).first
-    
-    unless garden.nil?
-      garden.users.each do |user|
-        next if user.phone.nil? or user.phone == from_number
-
-        @twilio_client.account.sms.messages.create(
-          :from => "+1#{to_number}",
-          :to => "#{user.phone}",
-          :body => message_body
-        )
-      end
-    end
-
-    render json: { :success => :true }
   end
 end
